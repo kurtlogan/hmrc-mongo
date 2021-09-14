39c2da5c09089c62aadd5fdab6c6f6433879f9f4
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2021-09-14 12:23:20 +0100

    BDOG-1445 Fix map/flatmap for observable

--- hmrc-mongo-play-27/src/main/scala/uk/gov/hmrc/mongo/transaction/Transactions.scala
+++ hmrc-mongo-play-27/src/main/scala/uk/gov/hmrc/mongo/transaction/Transactions.scala
@@ -22,6 +22,13 @@ import uk.gov.hmrc.mongo.MongoComponent
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration.DurationInt
+import java.util.concurrent.atomic.AtomicReference
+import org.mongodb.scala.internal.Init
+import org.mongodb.scala.internal.WaitingOnChild
+import org.mongodb.scala.internal.LastChildNotified
+import org.mongodb.scala.internal.LastChildResponded
+import org.mongodb.scala.internal.Done
+import org.mongodb.scala.internal.State
 
 
 /** Effectively provides the behaviour not available on `org.mongodb.scala.ClientSession` (driver-scala, nor
@@ -93,11 +100,11 @@ trait Transactions {
                          case e2 => logger.error(s"Error aborting transaction: ${e2.getMessage}", e2)
                                     Future.failed(e1)
                        }
-                       .flatMap { _ => Future.failed(e1) }
+                       .flatMap(_ => Future.failed(e1))
                    }
         _       <- retryFor(e =>
                      !e.isInstanceOf[MongoExecutionTimeoutException]
-                          && e.hasErrorLabel(MongoException.UNKNOWN_TRANSACTION_COMMIT_RESULT_LABEL)
+                     && e.hasErrorLabel(MongoException.UNKNOWN_TRANSACTION_COMMIT_RESULT_LABEL)
                    )(() => session.commitTransaction().toSingle().toFuture())
         } yield res
     }
@@ -140,32 +147,50 @@ trait Transactions {
   ): Observable[A] = {
     val startTimeMs = System.currentTimeMillis()
 
-    def retryFor[B](cond: MongoException => Boolean)(f: Observable[B]): Observable[B] =
-      f.recoverWith {
+    def retryFor[B](cond: MongoException => Boolean)(f: () => Observable[B]): Observable[B] =
+      f().recoverWith {
         case e: MongoException if cond(e) && System.currentTimeMillis() - startTimeMs < maxRetryTimeLimitMs =>
           logger.error(s"Failed with ${e.getLocalizedMessage} - will retry")
           retryFor(cond)(f)
       }
 
-    retryFor{_.hasErrorLabel(MongoException.TRANSIENT_TRANSACTION_ERROR_LABEL)}{
+    retryFor(_.hasErrorLabel(MongoException.TRANSIENT_TRANSACTION_ERROR_LABEL)){ () =>
       tc.transactionOptions.fold(session.startTransaction)(session.startTransaction _)
       for {
         res <- f.recoverWith { case e1 =>
-                 completeWith(session.abortTransaction(), {})
+                 completeWith(session.abortTransaction(), ())
                    .recover {
                      case e2 => logger.error(s"Error aborting transaction: ${e2.getMessage}", e2)
-                                throw e1
+                                failWith(e1) // throwing breaks MapObservable/FlatMapObservable...
                    }
-                   .map[A](_ => throw e1)
+                   .flatMap[A](_ => failWith(e1))  // throwing breaks MapObservable/FlatMapObservable...
                }
         _   <- retryFor(e =>
                  !e.isInstanceOf[MongoExecutionTimeoutException]
                  && e.hasErrorLabel(MongoException.UNKNOWN_TRANSACTION_COMMIT_RESULT_LABEL)
-               ){
-                 completeWith(session.commitTransaction(), ())
-               }
-        } yield res
+               )(() => completeWith(session.commitTransaction(), ()))
+      } yield res
     }
+  }
+
+  def failWith[A](e: Throwable) = new Observable[A] {
+    val delegate: Observable[Unit] = Observable[Unit](Seq(()))
+    override def subscribe(observer: Observer[_ >: A]): Unit =
+      delegate.subscribe(
+        new Observer[Unit] {
+          override def onError(throwable: Throwable): Unit =
+            observer.onError(throwable)
+
+          override def onSubscribe(sub: Subscription): Unit =
+            observer.onSubscribe(sub)
+
+          override def onComplete(): Unit =
+            () // not reached
+
+          override def onNext(tResult: Unit): Unit =
+            onError(e)
+        }
+      )
   }
 
   /** Observable[Void] by definition will never emit a value to be mapped (see org.mongodb.scala.internal.MapObservable).
@@ -193,6 +218,165 @@ trait Transactions {
           }
         )
     }
+
+  def wrap[A](observable: Observable[A]): Observable[A] = new Observable[A] {
+    override def subscribe(observer: Observer[_ >: A]): Unit =
+      observable.subscribe(observer)
+
+    override def map[S](mapFunction: A => S): Observable[S] =
+      MapObservable2(this, mapFunction)
+
+    override def flatMap[S](mapFunction: A => Observable[S]): Observable[S] =
+      FlatMapObservable2(this, mapFunction)
+   }
+
+   // fixes org.mongodb.scala.internal.MapObservable by not swallowing exceptions!
+   private case class MapObservable2[T, S](
+     observable: Observable[T],
+     s         : T => S,
+     f         : Throwable => Throwable = t => t
+   ) extends Observable[S] {
+     override def subscribe(observer: Observer[_ >: S]): Unit = {
+       observable.subscribe(
+         new Observer[T] {
+           override def onError(throwable: Throwable): Unit =
+             observer.onError(f(throwable))
+
+           override def onSubscribe(subscription: Subscription): Unit =
+             observer.onSubscribe(subscription)
+
+           override def onComplete(): Unit =
+             observer.onComplete()
+
+           override def onNext(tResult: T): Unit =
+             try {
+               observer.onNext(s(tResult))
+             } catch { // this is what differs from org.mongodb.scala.internal.MapObservable
+               case t: Throwable => observer.onError(f(t))
+             }
+         }
+       )
+     }
+   }
+
+   private case class FlatMapObservable2[T, S](
+     observable: Observable[T],
+     f: T => Observable[S]
+   ) extends Observable[S] {
+     override def subscribe(observer: Observer[_ >: S]): Unit = {
+       observable.subscribe(
+         new Observer[T] {
+           @volatile private var outerSubscription: Option[Subscription] = None
+           @volatile private var demand: Long = 0
+           private val state = new AtomicReference[State](Init)
+
+           override def onSubscribe(subscription: Subscription): Unit = {
+             val masterSub = new Subscription() {
+               override def isUnsubscribed: Boolean = subscription.isUnsubscribed
+               override def unsubscribe(): Unit = subscription.unsubscribe()
+               override def request(n: Long): Unit = {
+                 require(n > 0L, s"Number requested must be greater than zero: $n")
+                 val localDemand = addDemand(n)
+                 state.get() match {
+                   case Init              => subscription.request(1L)
+                   case WaitingOnChild(s) => s.request(localDemand)
+                   case _                 => // noop
+                 }
+               }
+             }
+             outerSubscription = Some(masterSub)
+             state.set(Init)
+             observer.onSubscribe(masterSub)
+           }
+
+          override def onComplete(): Unit =
+            state.get() match {
+              case Done  => // ok
+              case internal.Error => // ok
+              case Init if state.compareAndSet(Init, Done) =>
+                observer.onComplete()
+              case w @ WaitingOnChild(_) if state.compareAndSet(w, LastChildNotified) =>
+              // letting the child know that we delegate onComplete call to it
+              case LastChildNotified =>
+              // wait for the child to do the delegated onCompleteCall
+              case LastChildResponded if state.compareAndSet(LastChildResponded, Done) =>
+                observer.onComplete()
+              case other =>
+                // state machine is broken, let's fail
+                // normally this won't happen
+                throw new IllegalStateException(s"Unexpected state in FlatMapObservable `onComplete` handler: ${other}")
+            }
+
+          override def onError(throwable: Throwable): Unit =
+            observer.onError(throwable)
+
+          override def onNext(tResult: T): Unit =
+            try {
+              f(tResult)
+                .subscribe(
+                  new Observer[S]() {
+                    override def onError(throwable: Throwable): Unit = {
+                      state.set(internal.Error)
+                      observer.onError(throwable)
+                    }
+
+                    override def onSubscribe(subscription: Subscription): Unit = {
+                      state.set(WaitingOnChild(subscription))
+                      if (demand > 0) subscription.request(demand)
+                    }
+
+                    override def onComplete(): Unit = {
+                      state.get() match {
+                        case Done                                                                            => // no need to call parent's onComplete
+                        case internal.Error                                                                  => // no need to call parent's onComplete
+                        case LastChildNotified if state.compareAndSet(LastChildNotified, LastChildResponded) =>
+                          // parent told us to call onComplete
+                          observer.onComplete()
+                        case _ if demand > 0 =>
+                          // otherwise we are not the last child, let's tell the parent
+                          // it's not dealing with us anymore.
+                          // Init -> * will be handled by possible later items in the stream
+                          state.set(Init)
+                          addDemand(-1) // reduce demand by 1 as it will be incremented by the outerSubscription
+                          outerSubscription.foreach(_.request(1))
+                        case _ =>
+                          // no demand
+                          state.set(Init)
+                      }
+                    }
+
+                    override def onNext(tResult: S): Unit = {
+                      addDemand(-1)
+                      observer.onNext(tResult)
+                    }
+                  }
+                )
+              } catch { // this is what differs from org.mongodb.scala.internal.MapObservable
+                case t: Throwable => observer.onError(t)
+              }
+
+          /**
+          * Adds extra demand and protects against Longs rolling over
+          *
+          * @param extraDemand the amount of extra demand
+          * @return the updated demand
+          */
+          private def addDemand(extraDemand: Long): Long = {
+            this.synchronized {
+              demand += extraDemand
+              if (demand < 0) {
+                if (extraDemand < 0) {
+                  throw new IllegalStateException("Demand cannot be reduced to below zero")
+                }
+                demand = Long.MaxValue
+              }
+            }
+            demand
+          }
+        }
+       )
+     }
+   }
 }
 
 
--- hmrc-mongo-play-27/src/test/scala/uk/gov/hmrc/mongo/transacion/TransactionsSpec.scala
+++ hmrc-mongo-play-27/src/test/scala/uk/gov/hmrc/mongo/transacion/TransactionsSpec.scala
@@ -28,6 +28,9 @@ import uk.gov.hmrc.mongo.MongoUtils
 import java.util.concurrent.atomic.AtomicInteger
 import scala.concurrent.{ExecutionContext, Future}
 import ExecutionContext.Implicits.global
+import org.mongodb.scala.Observable
+import org.mongodb.scala.Subscription
+import org.mongodb.scala.Observer
 
 class TransactionSpec
   extends AnyWordSpecLike
@@ -42,111 +45,236 @@ class TransactionSpec
     MongoComponent(mongoUri = s"mongodb://localhost:27017/$databaseName")
   }
 
+  val collectionName = "myobject"
   val collection =
-    mongoComponent.database.getCollection[BsonDocument](collectionName = "myobject")
+    mongoComponent.database.getCollection[BsonDocument](collectionName = collectionName)
 
-  "Transaction" should {
-    "support Observable" in {
-      implicit val ts = TransactionConfiguration()
+  "Transactions" when {
+    "using Future" should {
+      "commit" in {
+        implicit val ts = TransactionConfiguration()
 
-      val res =
-        withSessionAndTransaction(session =>
-          for {
-            _   <- collection.insertOne(session, BsonDocument())
-            _   <- collection.insertOne(session, BsonDocument())
-            _   <- collection.deleteOne(session, BsonDocument())
-            res <- collection.find(session) // GOTCHA: if forget to pass session, it will silently stop..
-          } yield res
-        ).toFuture().futureValue
+        val res =
+          withSessionAndTransaction(session =>
+            for {
+              _   <- collection.insertOne(session, BsonDocument()).toFuture()
+              _   <- collection.insertOne(session, BsonDocument()).toFuture()
+              _   <- collection.deleteOne(session, BsonDocument()).toFuture()
+              res <- collection.find(session).toFuture() // GOTCHA: if forget to pass session, it will silently stop..
+            } yield res
+          ).futureValue
 
-      res.size shouldBe 1
+        res.size shouldBe 1
 
-      val list = collection.find().toFuture
-      list.futureValue.size shouldBe 1
-    }
+        val list = collection.find().toFuture
+        list.futureValue.size shouldBe 1
+      }
 
-    "support Future" in {
-      implicit val ts = TransactionConfiguration()
+      "rollback on error" in {
+        implicit val ts = TransactionConfiguration()
+        val attempts = new AtomicInteger(0)
 
-      val res =
-        withSessionAndTransaction(session =>
+        withSessionAndTransaction { session =>
+          attempts.incrementAndGet
           for {
             _   <- collection.insertOne(session, BsonDocument()).toFuture()
             _   <- collection.insertOne(session, BsonDocument()).toFuture()
-            _   <- collection.deleteOne(session, BsonDocument()).toFuture()
-            res <- collection.find(session).toFuture() // GOTCHA: if forget to pass session, it will silently stop..
-          } yield res
-        ).futureValue
+            _   =  sys.error("Fail")
+          } yield ()
+        }.failed.futureValue
 
-      res.size shouldBe 1
+        val list = collection.find().toFuture
+        list.futureValue.size shouldBe 0
 
-      val list = collection.find().toFuture
-      list.futureValue.size shouldBe 1
+        attempts.get shouldBe 1 // no retries
+      }
+
+      "retry on transient transaction errors" in {
+        implicit val ts = TransactionConfiguration()
+        val attempts = new AtomicInteger(0)
+
+        val e = new MongoException("Fail")
+        e.addLabel(MongoException.TRANSIENT_TRANSACTION_ERROR_LABEL)
+
+        withSessionAndTransaction { session =>
+          val i = attempts.incrementAndGet
+          for {
+            _   <- collection.insertOne(session, BsonDocument()).toFuture()
+            _   <- collection.insertOne(session, BsonDocument()).toFuture()
+            _   =  if (i == 1) throw e
+          } yield ()
+        }.futureValue
+
+        val list = collection.find().toFuture
+        list.futureValue.size shouldBe 2
+
+        attempts.get shouldBe 2 // retried once
+      }
     }
 
-    "rollback on error for Futures" in {
-      implicit val ts = TransactionConfiguration()
-      val attempts = new AtomicInteger(0)
+    "using Observable" should {
+      "commit" in {
+        implicit val ts = TransactionConfiguration()
 
-      withSessionAndTransaction { session =>
-        attempts.incrementAndGet
-        for {
-          _   <- collection.insertOne(session, BsonDocument()).toFuture()
-          _   <- collection.insertOne(session, BsonDocument()).toFuture()
-          _   =  sys.error("Fail")
-        } yield ()
-      }.failed.futureValue
+        val res =
+          withSessionAndTransaction(session =>
+            for {
+              _   <- collection.insertOne(session, BsonDocument())
+              _   <- collection.insertOne(session, BsonDocument())
+              _   <- collection.deleteOne(session, BsonDocument())
+              res <- collection.find(session) // GOTCHA: if forget to pass session, it will silently stop..
+            } yield res
+          ).toFuture().futureValue
 
-      val list = collection.find().toFuture
-      list.futureValue.size shouldBe 0
+        res.size shouldBe 1
 
-      attempts.get shouldBe 1 // no retries
+        val list = collection.find().toFuture
+        list.futureValue.size shouldBe 1
+      }
+
+      "rollback on error for failed Observables" in {
+        implicit val ts = TransactionConfiguration()
+        val attempts = new AtomicInteger(0)
+
+        withSessionAndTransaction { session =>
+          attempts.incrementAndGet
+          for {
+            _ <- collection.insertOne(session, BsonDocument())
+            _ <- collection.insertOne(session, BsonDocument())
+            _ <- failWith[Unit](new RuntimeException("Fail")) // doesn't work with `sys.error("Fail")`
+          } yield ()
+        }.toFuture().failed.futureValue
+
+        val list = collection.find().toFuture
+        list.futureValue.size shouldBe 0
+
+        attempts.get shouldBe 1 // no retries
+      }
+
+      "retry on transient transaction errors" in {
+        implicit val ts = TransactionConfiguration()
+        val attempts = new AtomicInteger(0)
+
+        val e = new MongoException("Fail")
+        e.addLabel(MongoException.TRANSIENT_TRANSACTION_ERROR_LABEL)
+
+        withSessionAndTransaction { session =>
+          val i = attempts.incrementAndGet
+          for {
+            _   <- collection.insertOne(session, BsonDocument())
+            _   <- collection.insertOne(session, BsonDocument())
+            _   <- if (i == 1)
+                    failWith[Unit](e) // doesn't work with `throw e`
+                  else Observable[Unit](Seq(()))
+          } yield ()
+        }.toFuture().futureValue
+
+        val list = collection.find().toFuture
+        list.futureValue.size shouldBe 2
+
+        attempts.get shouldBe 2 // retried once
+      }
+
+      // onRecover doesn't work for Observables?
+      // requires wrapping all observables with "wrap" to fix map/flatmap
+      /*"rollback on error for Observables" in {
+        implicit val ts = TransactionConfiguration()
+        val attempts = new AtomicInteger(0)
+
+        val e = new MongoException("Fail")
+        e.addLabel(MongoException.TRANSIENT_TRANSACTION_ERROR_LABEL)
+
+        val obs =
+          withSessionAndTransaction(session =>
+            for {
+              _ <- wrap(collection.insertOne(session, BsonDocument()))
+              _ <- wrap(collection.insertOne(session, BsonDocument()))
+              _ =  sys.error("Fail")
+            } yield ()
+          )
+
+        obs.toFuture().failed.futureValue
+
+        val list = collection.find().toFuture
+        list.futureValue.size shouldBe 0
+
+        attempts.get shouldBe 1 // no retries
+      }*/
+
+      // shows that wrap fixes Map and FlatMap
+      // TODO onRecover doesn't work for Observables?
+      /*"rollback on error for Observables2" in {
+        val successfulObservable = Observable[Int]((1 to 100).toStream)
+
+        val failedObservable = new Observable[Int] {
+          val delegate: Observable[Int] = Observable[Int]((1 to 100).toStream)
+          val failOn = 10
+          val errorMessage: String = "Failed"
+
+          override def subscribe(observer: Observer[_ >: Int]): Unit =
+                    delegate.subscribe(
+                      new Observer[Int] {
+                        var failed = false
+                        var subscription: Option[Subscription] = None
+                        override def onError(throwable: Throwable): Unit =
+                          observer.onError(throwable)
+
+                        override def onSubscribe(sub: Subscription): Unit =
+                          observer.onSubscribe(sub)
+
+                        override def onComplete(): Unit =
+                          if (!failed) observer.onComplete()
+
+                        override def onNext(tResult: Int): Unit = {
+                          /*if (!failed) {
+                            if (tResult == failOn) {
+                              failed = true
+                              onError(new MongoException(errorMessage))
+                            } else {
+                              observer.onNext(tResult)
+                            }
+                          }*/
+                          throw new MongoException(errorMessage)
+                        }
+                      }
+                    )
+        }
+
+
+        val obs =
+          // this goes into recover
+          //failedObservable
+
+          // this doesn't go into recover
+          //successfulObservable.map[Int](_ => sys.error("Fail2"))
+          // this does...
+          //wrap2(successfulObservable).map[Int](_ => sys.error("Fail2"))
+
+          // this doesn't go into recover
+          //successfulObservable.flatMap[Int](_ => sys.error("Fail2"))
+          wrap(successfulObservable).flatMap[Int](_ => sys.error("Fail2"))
+
+        // so... map & flatmap do not work as
+
+        val obs2 = obs.recover {
+          case t => println(s">>>>>>> Recover!!: $t"); throw t
+        }
+
+        import scala.concurrent.duration.DurationInt
+        scala.concurrent.Await.result(obs2.toFuture(), 10.seconds)
+
+        // Also: DocumentationTransactionsExampleSpec
+      }*/
     }
-
-    "retry on transient transaction errors for Futures" in {
-      implicit val ts = TransactionConfiguration()
-      val attempts = new AtomicInteger(0)
-
-      val e = new MongoException("Fail")
-      e.addLabel(MongoException.TRANSIENT_TRANSACTION_ERROR_LABEL)
-
-      withSessionAndTransaction { session =>
-        val i = attempts.incrementAndGet
-        for {
-          _   <- collection.insertOne(session, BsonDocument()).toFuture()
-          _   <- collection.insertOne(session, BsonDocument()).toFuture()
-          _   =  if (i == 1) throw e
-        } yield ()
-      }.futureValue
-
-      val list = collection.find().toFuture
-      list.futureValue.size shouldBe 2
-
-      attempts.get shouldBe 2 // retried once
-    }
-
-    // TODO onRecover doesn't work for Observables?
-    /*"rollback on error for Observables" in {
-      implicit val ts = TransactionConfiguration()
-
-      withSessionAndTransaction(session =>
-        for {
-          _   <- collection.insertOne(session, BsonDocument())
-          _   <- collection.insertOne(session, BsonDocument())
-          _   =  sys.error("Fail")
-        } yield ()
-      ).toFuture().failed.futureValue
-
-      val list = collection.find().toFuture
-      list.futureValue.size shouldBe 0
-    }*/
   }
 
   def prepareDatabase(): Unit =
     (for {
       exists <- MongoUtils.existsCollection(mongoComponent, collection)
       _      <- if (exists) collection.deleteMany(BsonDocument()).toFuture
-                else Future.successful(())
+                // until Mongo 4.4 implicit collection creation (on insert/upsert) will fail when in a transaction
+                // create explicitly
+                else mongoComponent.database.createCollection(collectionName).toFuture
      } yield ()
     ).futureValue
 
